ca05780e0ce3e5f07e653f52cb892a019b59e0ad
Toni Sissala <toni.sissala@tuni.fi>
2021-08-30 07:44:13 +0000

    Merged in bugfix/3-query-distinct-ids-filter (pull request #4)
    * Add filter condition to filter out logically deleted studies
    
    When querying for distinct studies to remove on upsert run, the
    distinct query should only return records which are not already
    logically removed. Add a filter condition to
    sync.StudyMethods.query_distinct_ids() which filters out logically
    removed studies.
    
    * Increase test coverage & add comments
    
    Add tests to assert behaviour with file log -functionality.
    Add tests to make sure sync.cli() calls its dependencies correctly.
    Add missing docstring comments.
    
    Issue #3 at Bitbucket.

--- cdcagg_client/sync.py
+++ cdcagg_client/sync.py
@@ -1,10 +1,15 @@
+"""Synchronize records to CESSDA CDCAggregator Document Store"""
 import sys
-from py12flogging.log_formatter import setup_app_logging
+import logging
 from kuha_common.query import (
     QueryController,
     FilterKeyConstants
 )
-from kuha_common import conf
+from kuha_common import (
+    conf,
+    cli_setup
+)
+from kuha_common.document_store.constants import REC_STATUS_DELETED
 import kuha_client
 from cdcagg_common.records import Study
 from cdcagg_common.mappings import (
@@ -14,11 +19,26 @@ from cdcagg_common.mappings import (
 )
 
 
+_logger = logging.getLogger(__name__)
+
+
 class StudyMethods(kuha_client.CollectionMethods):
+    """Implement StudyMethods subclass of CollectionMethods
+
+    Implement methods query_record and query_distinct_ids that
+    are abstract in base class.
+    Override method update_record to correctly handle provenance info.
+    """
 
     collection = Study.get_collection()
 
     async def query_record(self, record):
+        """Query record from Document Store.
+
+        :param :obj:`Study` record: Study to query for.
+        :returns: Result of the query.
+        :rtype: Instance of Study or None.
+        """
         return await QueryController().query_single(Study, _filter={
             FilterKeyConstants.and_: [
                 {Study._provenance.attr_direct: True},
@@ -26,14 +46,28 @@ class StudyMethods(kuha_client.CollectionMethods):
                 {Study._provenance.attr_base_url: record._provenance[-1].attr_base_url.get_value()}]})
 
     async def query_distinct_ids(self):
-        ids = await QueryController().query_distinct(Study, fieldname=Study._id)
+        """Query distinct IDs from collection that are not deleted.
+
+        :returns: Distinct ids
+        :rtype: set
+        """
+        ids = await QueryController().query_distinct(
+            Study, fieldname=Study._id,
+            _filter={Study._metadata.attr_status:
+                     {QueryController.fk_constants.not_equal: REC_STATUS_DELETED}})
         return set(ids[Study._id.path])
 
-    async def remove_record_by_id(self, _id):
-        return await kuha_client.send_delete_record_request(Study.get_collection(),
-                                                            record_id=_id)
-
     async def update_record(self, new, old):
+        """Update existing Document Store record.
+
+        Override :meth:`kuha_client.CollectionMethods.update_record`
+        to handle provenance data correctly.
+
+        :param :obj:`Study` new: New record.
+        :param :obj:`Study` old: Old record.
+        :returns: False if record does not need updating.
+        :rtype: bool
+        """
         new_dict = new.export_dict(include_provenance=False, include_metadata=False, include_id=False)
         old_dict = old.export_dict(include_provenance=False, include_metadata=False, include_id=False)
         if new_dict != old_dict:
@@ -55,21 +89,31 @@ class StudyMethods(kuha_client.CollectionMethods):
 
 
 def configure():
-    conf.load(prog='cdcagg_client.sync', package='cdcagg_client', env_var_prefix='CDCAGG_')
+    """Declare configuration options and load settings.
+
+    :returns: Loaded settings.
+    :rtype: :obj:`argparse.Namespace`
+    """
+    conf.load('cdcagg_client.sync', package='cdcagg_client', env_var_prefix='CDCAGG_')
     conf.add_print_arg()
     conf.add_config_arg()
-    conf.add_loglevel_arg()
-    conf.add('--document-store-url', type=str)
-    conf.add('--no-remove', action='store_true')
-    conf.add('--file-cache', type=str)
-    conf.add('paths', nargs='+')
-    settings = conf.get_conf()
-    setup_app_logging(conf.get_package(), loglevel=settings.loglevel)
+    conf.add('--no-remove', action='store_true', help="Don't remove records that were not found in this batch.")
+    conf.add('--file-cache', type=str, help='Path to a cache file. Leave unset to not use file caching.')
+    conf.add('paths', nargs='+', help="Paths to files to synchronize. If path points to a folder, it and its "
+             "subfolder will be searched for '.xml'-suffixed files")
+    settings = cli_setup.setup_common_modules(cli_setup.MOD_DS_CLIENT,
+                                              cli_setup.MOD_LOGGING,
+                                              cli_setup.MOD_DS_QUERY)
     return settings
 
 
-def cli():
-    settings = configure()
+def run(settings):
+    """Run the program with 'settings'.
+
+    Load :class:`BatchProcessor` and call :meth:`BatchProcessor.upsert_run`
+
+    :param :obj:`argparse.Namespace` settings: Use settings to run the program.
+    """
     remove_absent = settings.no_remove is False
     parsers = [DDI122NesstarRecordParser, DDI25RecordParser, DDI31RecordParser]
     collections_methods = [StudyMethods]
@@ -77,9 +121,29 @@ def cli():
         with kuha_client.open_file_logging_cache(settings.file_cache) as cache:
             proc = kuha_client.BatchProcessor(collections_methods, parsers=parsers, cache=cache)
             proc.upsert_run(settings.paths, remove_absent=remove_absent)
-        return 0
+        return
     proc = kuha_client.BatchProcessor(collections_methods, parsers=parsers)
     proc.upsert_run(settings.paths, remove_absent=remove_absent)
+
+
+def cli():
+    """Start the program from command line.
+
+    Load configuration, run program, log and
+    re-raise propagated exceptions.
+    """
+    settings = configure()
+    if settings.print_configuration:
+        print('Print active configuration and exit\n')
+        conf.print_conf()
+        return
+    try:
+        run(settings)
+    except KeyboardInterrupt:
+        _logger.warning('Shutdown by CTRL + C', exc_info=True)
+    except:
+        _logger.exception('Unhandled exception')
+        raise
 
 
 if __name__ == '__main__':
--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,29 +1,77 @@
 import os.path
-from unittest import (
-    TestCase,
-    mock
-)
+import tempfile
+from unittest import mock
 from argparse import Namespace
 from kuha_common.testing.testcases import KuhaUnitTestCase
 from kuha_common.document_store.constants import (
     REC_STATUS_CREATED,
-    REC_STATUS_DELETED
+    REC_STATUS_DELETED,
+    MDB_AND,
+    MDB_NOT_EQUAL
+)
+from kuha_common.cli_setup import (
+    MOD_DS_CLIENT,
+    MOD_DS_QUERY,
+    MOD_LOGGING
 )
 from cdcagg_common.records import Study
 from cdcagg_client import sync
 
 
-class TestConfigure(TestCase):
+def _testdata_path(path=''):
+    if path:
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testdata', path)
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testdata')
 
-    @mock.patch.object(sync.conf, 'get_conf')
-    @mock.patch.object(sync.conf, 'get_package')
-    @mock.patch.object(sync.conf, 'add_loglevel_arg')
-    @mock.patch.object(sync.conf, 'add')
-    @mock.patch.object(sync.conf, 'load')
-    def test_calls_conf_load(self, mock_load, mock_add, mock_add_loglevel_arg, mock_get_package, mock_get_conf):
-        mock_get_package.return_value = 'cdcagg_client'
-        mock_get_conf.return_value = Namespace(loglevel='INFO')
+
+def settings(paths, no_remove=False, file_cache='', **kw):
+    return Namespace(paths=paths,
+                     no_remove=no_remove,
+                     file_cache=file_cache,
+                     print_configuration=kw.get('print_configuration', False))
+
+
+class TestConfigure(KuhaUnitTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self._mock_conf = self.init_patcher(mock.patch.object(sync, 'conf'))
+        self._mock_setup_common_modules = self.init_patcher(mock.patch.object(
+            sync.cli_setup, 'setup_common_modules'))
+
+    def test_calls_conf_load(self):
         sync.configure()
+        self._mock_conf.load.assert_called_once_with('cdcagg_client.sync',
+                                                     package='cdcagg_client',
+                                                     env_var_prefix='CDCAGG_')
+
+    def test_calls_conf_add_print_arg(self):
+        sync.configure()
+        self._mock_conf.add_print_arg.assert_called_once_with()
+
+    def test_calls_conf_add_config_arg(self):
+        sync.configure()
+        self._mock_conf.add_config_arg.assert_called_once_with()
+
+    def test_calls_conf_add_correctly(self):
+        sync.configure()
+        self._mock_conf.add.assert_has_calls([
+            mock.call('--no-remove', action='store_true',
+                      help="Don't remove records that were not found in this batch."),
+            mock.call('--file-cache', type=str,
+                      help='Path to a cache file. Leave unset to not use file caching.'),
+            mock.call('paths', nargs='+',
+                      help="Paths to files to synchronize. If path points to a folder, "
+                      "it and its subfolder will be searched for '.xml'-suffixed files")])
+
+    def test_calls_cli_setup_setup_common_modules(self):
+        sync.configure()
+        self._mock_setup_common_modules.assert_called_once_with(
+            MOD_DS_CLIENT, MOD_LOGGING, MOD_DS_QUERY)
+
+    def test_returns_setup_common_modules_rval(self):
+        rval = sync.configure()
+        self.assertEqual(rval, self._mock_setup_common_modules.return_value)
 
 
 class TestStudyMethods(KuhaUnitTestCase):
@@ -86,10 +134,35 @@ class TestStudyMethods(KuhaUnitTestCase):
         self.assertEqual(self._stored_result, True)
 
 
-def _testdata_path(path=''):
-    if path:
-        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testdata', path)
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testdata')
+class TestCli(KuhaUnitTestCase):
+
+    @mock.patch.object(sync, 'run')
+    @mock.patch.object(sync, 'configure', return_value=settings([]))
+    def test_calls_configure(self, mock_configure, mock_run):
+        sync.cli()
+        mock_configure.assert_called_once_with()
+
+    @mock.patch.object(sync, 'run')
+    @mock.patch.object(sync, 'configure', return_value=settings(['/some/path']))
+    def test_calls_run(self, mock_configure, mock_run):
+        sync.cli()
+        mock_run.assert_called_once_with(settings(['/some/path']))
+
+    @mock.patch.object(sync.conf, 'print_conf')
+    @mock.patch.object(sync, 'run')
+    @mock.patch.object(sync, 'configure', return_value=settings(['/some/path'], print_configuration=True))
+    def test_prints_configuration(self, mock_configure, mock_run, mock_print_conf):
+        sync.cli()
+        mock_print_conf.assert_called_once_with()
+        mock_run.assert_not_called()
+
+    @mock.patch.object(sync, '_logger')
+    @mock.patch.object(sync, 'run', side_effect=ValueError())
+    @mock.patch.object(sync, 'configure', return_value=settings(['/some/path']))
+    def logs_out_exceptions_from_run(self, mock_configure, mock_run, mock_logger):
+        with self.assertRaises(ValueError):
+            sync.cli()
+        mock_logger.exception.assert_called_once_with('Unhandled exception')
 
 
 class TestIntegration(KuhaUnitTestCase):
@@ -106,14 +179,28 @@ class TestIntegration(KuhaUnitTestCase):
         self._mock_send_create_record_request = self.init_patcher(mock.patch('kuha_client.send_create_record_request'))
         self._mock_configure = self.init_patcher(mock.patch.object(sync, 'configure'))
 
-    def test_minimal_ddi122(self):
+    def test_minimal_ddi122_calls_query_single_correctly(self):
         self._mock_query_single.return_value = None
-        self._mock_configure.return_value = Namespace(paths=[_testdata_path('minimal_ddi122.xml')],
-                                                      no_remove=False,
-                                                      file_cache='')
+        self._mock_configure.return_value = settings([_testdata_path('minimal_ddi122.xml')])
         # Call
         sync.cli()
         # Assert
+        self._mock_query_single.assert_called_once_with(
+            Study, _filter={MDB_AND: [
+                {Study._provenance.attr_direct: True},
+                {Study._provenance.attr_identifier: 'oai:fsd.uta.fi:FSD0115'},
+                {Study._provenance.attr_base_url: 'http://services.fsd.tuni.fi/v0/oai'}
+            ]}
+        )
+
+    def test_minimal_ddi122_creates(self):
+        self._mock_query_single.return_value = None
+        self._mock_configure.return_value = settings([_testdata_path('minimal_ddi122.xml')])
+        # Call
+        sync.cli()
+        # Assert
+        self._mock_send_delete_record_request.assert_not_called()
+        self._mock_send_update_record_request.assert_not_called()
         calls = self._mock_send_create_record_request.call_args_list
         self.assertEqual(len(calls), 1)
         cargs, ckwargs = calls.pop()
@@ -123,3 +210,82 @@ class TestIntegration(KuhaUnitTestCase):
         self.assertEqual(coll, Study.get_collection())
         self.assertEqual(rec_dict['study_number'], 'study_1')
         self.assertEqual(rec_dict['study_titles'][0]['study_title'], 'some study')
+
+    def test_minimal_ddi122_updates(self):
+        self._mock_query_single.return_value = Study({'study_number': 'study_1',
+                                                      'study_titles': [
+                                                          {'study_title': 'old title',
+                                                           'language': 'en'}],
+                                                      '_id': 'some_id'})
+        self._mock_configure.return_value = settings([_testdata_path('minimal_ddi122.xml')])
+        # Call
+        sync.cli()
+        # Assert
+        self._mock_send_delete_record_request.assert_not_called()
+        self._mock_send_create_record_request.assert_not_called()
+        calls = self._mock_send_update_record_request.call_args_list
+        self.assertEqual(len(calls), 1)
+        cargs, ckwargs = calls.pop()
+        self.assertEqual(ckwargs, {})
+        self.assertEqual(len(cargs), 3)
+        coll, rec_dict, rec_id = cargs
+        self.assertEqual(coll, Study.get_collection())
+        self.assertEqual(rec_dict['study_number'], 'study_1')
+        self.assertEqual(rec_dict['study_titles'][0]['study_title'], 'some study')
+        self.assertEqual(rec_id, 'some_id')
+
+    def test_minimal_ddi122_calls_query_distinct_correctly(self):
+        self._mock_query_single.return_value = None
+        self._mock_configure.return_value = settings([_testdata_path('minimal_ddi122.xml')])
+        sync.cli()
+        self._mock_query_distinct.assert_called_once_with(
+            Study, fieldname=Study._id, _filter={
+                Study._metadata.attr_status: {MDB_NOT_EQUAL: REC_STATUS_DELETED}
+            }
+        )
+
+    def test_minimal_ddi122_deletes(self):
+        self._mock_query_single.return_value = None
+        self._mock_query_distinct.return_value = {'_id': ['delete_me_1', 'delete_me_2']}
+        self._mock_configure.return_value = settings([_testdata_path('minimal_ddi122.xml')])
+        sync.cli()
+        self._mock_send_delete_record_request.assert_has_calls([
+            mock.call('studies', record_id='delete_me_1'),
+            mock.call('studies', record_id='delete_me_2')
+        ], any_order=True)
+
+    def test_minimal_ddi122_with_file_log_calls_send_update_request_correctly(self):
+        """First call with nonexistent file cache will call docstore normally. Second call
+        with a up-to-date filelog will not consult document store at all."""
+        self._mock_query_single.return_value = Study({'study_number': 'study_1',
+                                                      '_id': 'some_id'})
+        with tempfile.TemporaryDirectory() as dirname:
+            cache_path = os.path.join(dirname, 'filelog')
+            self._mock_configure.return_value = settings([_testdata_path('minimal_ddi122.xml')],
+                                                         file_cache=cache_path)
+            # FIRST CALL
+            sync.cli()
+            self._mock_query_single.assert_called_once_with(
+                Study, _filter={MDB_AND: [
+                    {Study._provenance.attr_direct: True},
+                    {Study._provenance.attr_identifier: 'oai:fsd.uta.fi:FSD0115'},
+                    {Study._provenance.attr_base_url: 'http://services.fsd.tuni.fi/v0/oai'}
+                ]}
+            )
+            calls = self._mock_send_update_record_request.call_args_list
+            self.assertEqual(len(calls), 1)
+            cargs, ckwargs = calls.pop()
+            self.assertEqual(ckwargs, {})
+            self.assertEqual(len(cargs), 3)
+            coll, rec_dict, rec_id = cargs
+            self.assertEqual(coll, Study.get_collection())
+            self.assertEqual(rec_dict['study_number'], 'study_1')
+            self.assertEqual(rec_dict['study_titles'][0]['study_title'], 'some study')
+            self.assertEqual(rec_id, 'some_id')
+            # SECOND CALL
+            self._mock_send_update_record_request.reset_mock()
+            self._mock_query_single.reset_mock()
+            sync.cli()
+            # ASSERT
+            self._mock_send_update_record_request.assert_not_called()
+            self._mock_query_single.assert_not_called()
